94ca818fdb67fa9f3f0e401cb33657357d7c0530
LAFLAME55 <serkan.tugra@swisscom.com>
2021-10-26 11:46:38 +0200

    Added stepper

--- Rainbow6-app/src/app/dialog/store-dialog/store-dialog.component.ts
+++ Rainbow6-app/src/app/dialog/store-dialog/store-dialog.component.ts
@@ -8,19 +8,18 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styleUrls: ['./store-dialog.component.scss']
 })
 export class StoreDialogComponent implements OnInit {
-  firstFormGroup: FormGroup | any;
-  secondFormGroup: FormGroup | any;
-  isEditable = false;
+  // firstFormGroup: FormGroup | any;
+  // secondFormGroup: FormGroup | any;
+  // isEditable = false;
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+    // this.firstFormGroup = this._formBuilder.group({
+    //   firstCtrl: ['', Validators.required]
+    // });
+    // this.secondFormGroup = this._formBuilder.group({
+    //   secondCtrl: ['', Validators.required]
+    // });
   }
-
 }
